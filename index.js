a1fd12439a31625fa9c6a29cb7908fb7b62f302b
saarakhalid <khalidsaara004@gmail.com>
2023-12-21 00:44:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100); 
+
+const num3 = 10;
+const num4 = 3;
+const mod = (num3 % num4) + num4;
+
+const max = Math.max(5, 10, 15, 20);
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
